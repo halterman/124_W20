176add96f96a4098489b0c7ca6b019b0ec1839f8
Rick Halterman <halterman@southern.edu>
2020-01-15 17:06:25 -0500

    Version: Wed Jan 15 17:06:25 EST 2020

---
+++ Box/src/Box.java
@@ -0,0 +1,12 @@
+
+public class Box {
+
+	public static void main(String[] args) {
+		System.out.println("* * * * * * *");
+		System.out.println("*           *");
+		System.out.println("*           *");
+		System.out.println("*           *");
+		System.out.println("*           *");
+		System.out.println("* * * * * * *");
+	}
+}
